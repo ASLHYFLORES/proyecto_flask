89ddc58fc2f64b5679d17b4f4d66cd50e41820a0
Aslhy <ay.floresn@uea.edu.ec>
2026-02-25 18:08:14 -0500

    Proyecto final con POO y diseño mejorado

--- app.py
+++ app.py
@@ -1,80 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-import os
+from logica import Inventario
 
 app = Flask(__name__)
-DB_NAME = 'reposteria.db'
-
-# --- CLASES (POO) ---
-class ProductoPostre:
-    def __init__(self, id, nombre, categoria, stock, precio):
-        self.id = id
-        self.nombre = nombre
-        self.categoria = categoria
-        self.stock = stock
-        self.precio = precio
-
-# --- RUTAS ---
+gestion = Inventario()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Vista Pública (El docente solo ve esto)
 @app.route('/productos')
 def ver_productos():
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos")
-    filas = cursor.fetchall()
-    conn.close()
-    postres = [ProductoPostre(*f) for f in filas]
-    return render_template('productos.html', postres=postres, admin=False)
+    lista = gestion.cargar_desde_db()
+    return render_template('productos.html', postres=lista, admin=False)
 
-# Vista Privada (Tu ruta secreta para gestionar)
 @app.route('/admin')
 def admin_productos():
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos")
-    filas = cursor.fetchall()
-    conn.close()
-    postres = [ProductoPostre(*f) for f in filas]
-    return render_template('productos.html', postres=postres, admin=True)
+    lista = gestion.cargar_desde_db()
+    return render_template('productos.html', postres=lista, admin=True)
 
 @app.route('/nuevo_postre', methods=['POST'])
 def nuevo_postre():
-    nombre = request.form['nombre']
-    categoria = request.form['categoria']
-    stock = request.form['stock']
-    precio = request.form['precio']
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO productos (nombre, categoria, stock, precio) VALUES (?, ?, ?, ?)",
-                   (nombre, categoria, stock, precio))
-    conn.commit()
-    conn.close()
+    gestion.añadir(request.form['nombre'], request.form['categoria'], 
+                   request.form['stock'], request.form['precio'])
     return redirect(url_for('admin_productos'))
 
 @app.route('/eliminar/<int:id>')
 def eliminar(id):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM productos WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
+    gestion.eliminar(id)
     return redirect(url_for('admin_productos'))
 
 @app.route('/buscar', methods=['POST'])
 def buscar():
-    consulta = request.form.get('consulta', '')
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos WHERE nombre LIKE ?", ('%' + consulta + '%',))
-    filas = cursor.fetchall()
-    conn.close()
-    postres = [ProductoPostre(*f) for f in filas]
-    return render_template('productos.html', postres=postres, admin=False)
+    query = request.form.get('consulta', '')
+    lista = gestion.buscar(query)
+    return render_template('productos.html', postres=lista, admin=False)
 
 @app.route('/acerca')
 def acerca():
--- logica.py
+++ logica.py
@@ -2,30 +2,48 @@ import sqlite3
 
 class Producto:
     def __init__(self, id, nombre, categoria, stock, precio):
-        self.id = id
-        self.nombre = nombre
-        self.categoria = categoria
-        self.stock = stock
-        self.precio = precio
+        self._id = id
+        self._nombre = nombre
+        self._categoria = categoria
+        self._stock = stock
+        self._precio = precio
+
+    # --- Requisito: Getters (para obtener datos) ---
+    @property
+    def id(self): return self._id
+    @property
+    def nombre(self): return self._nombre
+    @property
+    def categoria(self): return self._categoria
+    @property
+    def stock(self): return self._stock
+    @property
+    def precio(self): return self._precio
+
+    # --- Requisito: Setters (para actualizar datos) ---
+    @stock.setter
+    def stock(self, nuevo_valor): self._stock = nuevo_valor
+    @precio.setter
+    def precio(self, nuevo_valor): self._precio = nuevo_valor
 
 class Inventario:
     def __init__(self):
         self.db_name = 'reposteria.db'
-        # Usamos un DICCIONARIO para cumplir con el requisito
-        self.productos_dict = {}
+        self.productos_dict = {} # REQUISITO: Uso de Diccionario
 
     def conectar(self):
         return sqlite3.connect(self.db_name)
 
     def cargar_desde_db(self):
-        """Carga los datos de SQLite al Diccionario de la clase"""
+        """Carga datos de SQLite al Diccionario del programa"""
         conn = self.conectar()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM productos")
         filas = cursor.fetchall()
-        # Llenamos el diccionario: { id: Objeto_Producto }
+        # Llenamos el diccionario usando el ID como llave
         self.productos_dict = {f[0]: Producto(*f) for f in filas}
         conn.close()
+        return self.productos_dict.values()
 
     def añadir(self, nombre, categoria, stock, precio):
         conn = self.conectar()
@@ -34,22 +52,18 @@ class Inventario:
                        (nombre, categoria, stock, precio))
         conn.commit()
         conn.close()
-        self.cargar_desde_db()
 
-    def eliminar(self, id_producto):
+    def eliminar(self, id_p):
         conn = self.conectar()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM productos WHERE id = ?", (id_producto,))
+        cursor.execute("DELETE FROM productos WHERE id = ?", (id_p,))
         conn.commit()
         conn.close()
-        if id_producto in self.productos_dict:
-            del self.productos_dict[id_producto]
 
-    def actualizar(self, id_producto, nuevo_stock, nuevo_precio):
+    def buscar(self, nombre_buscado):
         conn = self.conectar()
         cursor = conn.cursor()
-        cursor.execute("UPDATE productos SET stock = ?, precio = ? WHERE id = ?", 
-                       (nuevo_stock, nuevo_precio, id_producto))
-        conn.commit()
+        cursor.execute("SELECT * FROM productos WHERE nombre LIKE ?", ('%' + nombre_buscado + '%',))
+        filas = cursor.fetchall()
         conn.close()
-        self.cargar_desde_db()
+        return [Producto(*f) for f in filas]
